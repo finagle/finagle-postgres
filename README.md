4095c3183a287627ebd530130fc544c13224295e
Mairbek Khadikov <mkhadikov@gmail.com>
2012-09-30 15:08:57 +0300

    Configured publishing to maven

--- README.md
+++ README.md
@@ -13,3 +13,28 @@ Postgres database support for finagle.
     }
 
     logger.debug("Responded " + f.get)
+
+
+##### Adding dependencies
+
+Maven
+
+  <repositories>
+    <repository>
+       <id>twitterrepo</id>
+       <name>twitter-repo</name>
+       <url>http://maven.twttr.com/</url>
+       <layout>default</layout>
+     </repository>
+  </repositories>
+
+  <dependency>
+    <groupId>com.github.mairbek</groupId>
+    <artifactId>finagle-postgres_2.9.2</artifactId>
+    <version>0.0.1-SNAPSHOT</version>
+    <scope>compile</scope>
+  </dependency>
+
+sbt
+  
+  "com.github.mairbek" % "finagle-postgres" % "0.0.1-SNAPSHOT"
--- project/Build.scala
+++ project/Build.scala
@@ -7,17 +7,37 @@ object FinaglePostgres extends Build {
   val baseSettings = Defaults.defaultSettings ++ Seq(resolvers += "twitter-repo" at "http://maven.twttr.com",
     libraryDependencies ++= Seq(
       "com.twitter" % "finagle-core" % "5.3.0",
-      
       "com.twitter" % "util-logging" % "5.3.6"
     ))
 
-  lazy val buildSettings = Seq(organization := "Mairbek Khadikov",
+  lazy val buildSettings = Seq(
+    organization := "com.github.mairbek",
     version := "0.0.1-SNAPSHOT",
     scalaVersion := "2.9.2"
   )
 
+  lazy val publishSettings = Seq(
+    publishMavenStyle := true,
+    publishArtifact := true,
+    publishTo := Some(Resolver.file("localDirectory",  file("/Users/mkhadikov/Projects/personal/mvn-repo"))),
+    licenses := Seq("Apache 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
+    homepage := Some(url("https://github.com/mairbek/finagle-postgres")),
+    pomExtra := (
+      <scm>
+        <url>git://github.com/mairbek/finagle-postgres.git</url>
+        <connection>scm:git://github.com/mairbek/finagle-postgres.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>mairbek</id>
+          <name>Mairbek Khadikov</name>
+          <url>http://github.com/mairbek</url>
+        </developer>
+    </developers>)
+  )
+
   lazy val root = Project(id = "finagle-postgres",
     base = file("."),
-    settings = baseSettings ++ buildSettings)
+    settings = baseSettings ++ buildSettings ++ publishSettings)
 
 }
