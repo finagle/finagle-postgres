8f88b86274a1fb1f76b0bb7b7cc6585d2d2a8b61
Mairbek Khadikov <mkhadikov@gmail.com>
2012-11-06 23:31:27 +0200

    finagle-postgres 0.0.1 release

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object FinaglePostgres extends Build {
 
   lazy val buildSettings = Seq(
     organization := "com.github.mairbek",
-    version := "0.0.1-SNAPSHOT",
+    version := "0.0.1",
     scalaVersion := "2.9.2"
   )
 
