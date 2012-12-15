269c2cc793fbaf2675b622ed16b22eee03337e87
Mairbek Khadikov <mkhadikov@gmail.com>
2012-12-15 14:36:25 +0200

    Configured integration testing

--- project/Build.scala
+++ project/Build.scala
@@ -6,8 +6,8 @@ object FinaglePostgres extends Build {
 
   val baseSettings = Defaults.defaultSettings ++ Seq(resolvers += "twitter-repo" at "http://maven.twttr.com",
     libraryDependencies ++= Seq(
-      "org.specs2" %% "specs2" % "1.12.2" % "test",
-      "junit" % "junit" % "4.7"	% "test",
+      "org.specs2" %% "specs2" % "1.12.2" % "it,test",
+      "junit" % "junit" % "4.7" % "test, it",
       "com.twitter" % "finagle-core" % "5.3.0",
       "com.twitter" % "util-logging" % "5.3.6"
     ))
@@ -21,7 +21,7 @@ object FinaglePostgres extends Build {
   lazy val publishSettings = Seq(
     publishMavenStyle := true,
     publishArtifact := true,
-    publishTo := Some(Resolver.file("localDirectory",  file(Path.userHome.absolutePath + "/Projects/personal/mvn-repo"))),
+    publishTo := Some(Resolver.file("localDirectory", file(Path.userHome.absolutePath + "/Projects/personal/mvn-repo"))),
     licenses := Seq("Apache 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
     homepage := Some(url("https://github.com/mairbek/finagle-postgres")),
     pomExtra := (
@@ -29,17 +29,18 @@ object FinaglePostgres extends Build {
         <url>git://github.com/mairbek/finagle-postgres.git</url>
         <connection>scm:git://github.com/mairbek/finagle-postgres.git</connection>
       </scm>
-      <developers>
-        <developer>
-          <id>mairbek</id>
-          <name>Mairbek Khadikov</name>
-          <url>http://github.com/mairbek</url>
-        </developer>
-    </developers>)
+        <developers>
+          <developer>
+            <id>mairbek</id>
+            <name>Mairbek Khadikov</name>
+            <url>http://github.com/mairbek</url>
+          </developer>
+        </developers>)
   )
 
   lazy val root = Project(id = "finagle-postgres",
     base = file("."),
-    settings = baseSettings ++ buildSettings ++ publishSettings)
+    settings = Defaults.itSettings ++ baseSettings ++ buildSettings ++ publishSettings)
+      .configs( IntegrationTest)
 
 }
